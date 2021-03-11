6ff8eca2c1a344929a9055c41894b7d649dbb859
Duraid Maihoub <73091719+xDuraid@users.noreply.github.com>
2021-03-10 20:04:57 -0600

    Create 112A - Petya and Strings.cpp

---
+++ 112A - Petya and Strings.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s1,s2;
+    cin >> s1 >> s2;
+    for(int i = 0; i < s1.size(); i++){
+        if(tolower(s1[i]) == tolower(s2[i]))
+            continue;
+        else if(tolower(s1[i]) > tolower(s2[i])){
+            cout << "1";
+            return 0;
+        }
+        else{
+            cout << "-1";
+            return 0;
+        }
+    }
+    cout << "0";
+    return 0;
+}
