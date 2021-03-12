71243823e9710b1ac9fa958439e331fc97d84b3f
Duraid Maihoub <73091719+xDuraid@users.noreply.github.com>
2021-03-12 01:06:05 -0600

    Update 236A - Boy or Girl.cpp

--- 236A - Boy or Girl.cpp
+++ 236A - Boy or Girl.cpp
@@ -1,20 +1,15 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 int main()
 {
     string s;
-    int a[128] = {0};
-    int sum = 0;
     cin >> s;
-    for(int i = 0; i < s.size(); i++){
-        a[s[i]] = 1;
-    }
-    for(int i = 0; i < 128; i++){
-        sum += a[i];
-    }
-    if(sum % 2 == 0)
+    sort(s.begin(),s.end());
+    s.erase(unique(s.begin(),s.end()),s.end());
+    if(s.size() % 2 == 0)
         cout << "CHAT WITH HER!";
     else
         cout << "IGNORE HIM!";
