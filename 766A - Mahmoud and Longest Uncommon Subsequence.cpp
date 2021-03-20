b3129ee29238247196f984f2f9a471137ea3f645
Duraid Maihoub <73091719+xDuraid@users.noreply.github.com>
2021-03-20 14:27:05 -0400

    Create 766A - Mahmoud and Longest Uncommon Subsequence.cpp

---
+++ 766A - Mahmoud and Longest Uncommon Subsequence.cpp
@@ -0,0 +1,14 @@
+include <iostream>
+#include <string>
+using namespace std;
+ 
+int main()
+{
+    string a,b;
+    cin >> a >> b;
+    if(a == b)
+        cout << -1;
+    else
+        cout << max(a.size(),b.size());
+    return 0;
+}
