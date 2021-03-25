cbc9fa7fc6b75b16f7eef517a717eda3614c6e90
Duraid Maihoub <73091719+xDuraid@users.noreply.github.com>
2021-03-25 09:22:57 -0400

    Create 768A - Oath of the Night's Watch.cpp

---
+++ 768A - Oath of the Night's Watch.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 0, r = 0;
+    cin >> n;
+    int a[n] = {0};
+    int mx = 0, mn = INT_MAX;
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+        if(a[i] > mx)
+            mx = a[i];
+        if(a[i] < mn)
+            mn = a[i];
+    }
+    for(int i = 0; i < n; i++){
+        if(a[i] < mx && a[i] > mn)
+            r++;
+    }
+    cout << r;
+    return 0;
+}
