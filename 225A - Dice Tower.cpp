e7bfe95fac6ed283b66205fce53b6fcce46eee09
Duraid Maihoub <73091719+xDuraid@users.noreply.github.com>
2021-04-04 23:00:59 -0400

    Update 225A - Dice Tower.cpp

--- 225A - Dice Tower.cpp
+++ 225A - Dice Tower.cpp
@@ -3,19 +3,15 @@ using namespace std;
 
 int main()
 {
-    int n = 0, a = 0, b = 0, c = 0;
-    int down = 0;
-    cin >> n;
-    cin >> a >> b >> c;
-    n--;
-    down = 7 - a;
+    int n = 0, a = 0, b = 0, up = 0;
+    cin >> n >> up;
     for(int i = 0; i < n; i++){
-        cin >> b >> c;
-        if(down == b || down == 7 - b || down == c || down == 7 - c){
+        cin >> a >> b;
+        if(up == a || up == 7 - a || up == b || up == 7 - b){
             cout << "NO";
             return 0;
         }
-        down = 7 - (21 - (14+down));
+        up = 7 - up;
     }
     cout << "YES";
     return 0;
