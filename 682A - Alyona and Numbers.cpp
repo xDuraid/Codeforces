8ac84b58e3d589967bbfe41f45239fd794d1ebdc
Duraid Maihoub <73091719+xDuraid@users.noreply.github.com>
2021-04-06 20:58:21 -0400

    Update 682A - Alyona and Numbers.cpp

--- 682A - Alyona and Numbers.cpp
+++ 682A - Alyona and Numbers.cpp
@@ -5,17 +5,17 @@ int main()
 {
     long long n = 0, m = 0;
     cin >> n >> m;
-    long long a[5] = {0}, b[5] = {0};
+    long long rem[5] = {0}, need[6] = {0};
     for(int i = 1; i <= n; i++){
-        a[i % 5]++;
+        need[5 - i % 5]++;
     }
     for(int i = 1; i <= m; i++){
-        b[i % 5]++;
+        rem[i % 5]++;
     }
-    cout << a[0]*b[0]+
-            a[1]*b[4]+
-            a[2]*b[3]+
-            a[3]*b[2]+
-            a[4]*b[1];
+    cout << need[5]*rem[0]+
+            need[1]*rem[1]+
+            need[2]*rem[2]+
+            need[3]*rem[3]+
+            need[4]*rem[4];
     return 0;
 }
