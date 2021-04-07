79516296661bd8c289e0210184605537dbbb5434
Duraid Maihoub <73091719+xDuraid@users.noreply.github.com>
2021-04-06 20:51:12 -0400

    Create 682A - Alyona and Numbers.cpp

---
+++ 682A - Alyona and Numbers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long n = 0, m = 0;
+    cin >> n >> m;
+    long long a[5] = {0}, b[5] = {0};
+    for(int i = 1; i <= n; i++){
+        a[i % 5]++;
+    }
+    for(int i = 1; i <= m; i++){
+        b[i % 5]++;
+    }
+    cout << a[0]*b[0]+
+            a[1]*b[4]+
+            a[2]*b[3]+
+            a[3]*b[2]+
+            a[4]*b[1];
+    return 0;
+}
