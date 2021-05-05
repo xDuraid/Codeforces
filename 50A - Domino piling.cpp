88851f505b386a5c4ec6e922d3e5275a4222ea1e
Duraid Maihoub <73091719+xDuraid@users.noreply.github.com>
2021-05-04 20:46:49 -0400

    Create 50A - Domino piling.cpp

---
+++ 50A - Domino piling.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+#define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+int main()
+{
+    IOS
+    int n = 0, m = 0;
+    cin >> n >> m;
+    cout << (n*m)/2;
+    return 0;
+}
