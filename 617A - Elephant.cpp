14fb38ef6b663ad7db3c5246370d34b3d2b69894
Duraid Maihoub <73091719+xDuraid@users.noreply.github.com>
2021-05-04 21:27:05 -0400

    Create 617A - Elephant.cpp

---
+++ 617A - Elephant.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+#define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+int main()
+{
+    IOS
+    int x = 0;
+    cin >> x;
+    cout << (x + 4)/5;
+    return 0;
+}
