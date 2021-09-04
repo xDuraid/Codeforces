63f96c2bbb2281ccb23356e8446979afc792ce75
Duraid Maihoub <73091719+xDuraid@users.noreply.github.com>
2021-09-04 11:14:43 -0600

    Create 758A - Holiday Of Equality.cpp

---
+++ 758A - Holiday Of Equality.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+#define IOS ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define TEST int t = 0; cin >> t; while(t--)
+
+int main()
+{
+    IOS
+    int n = 0, mx = 0, ans = 0;
+    cin >> n;
+    int a[n] = {0};
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+        mx = max(mx, a[i]);
+    }
+    for(int i = 0; i < n; i++)
+        ans += mx - a[i];
+    cout << ans;
+    return 0;
+}
