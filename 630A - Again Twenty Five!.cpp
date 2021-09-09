4340e9f73f784be9216ca6cf9e26014266f0a295
Duraid Maihoub <73091719+xDuraid@users.noreply.github.com>
2021-09-09 11:11:26 -0600

    Create 630A - Again Twenty Five!.cpp

---
+++ 630A - Again Twenty Five!.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+#define IOS ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define TEST int t = 0; cin >> t; while(t--)
+
+
+int main()
+{
+    IOS
+    long long n = 0;
+    cin >> n;
+    cout << 25;
+    return 0;
+}
