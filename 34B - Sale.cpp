af3c082b55614ed42b180ba6fc38fa0d62460e2b
Duraid Maihoub <73091719+xDuraid@users.noreply.github.com>
2021-09-11 14:08:51 -0600

    Update 34B - Sale.cpp

--- 34B - Sale.cpp
+++ 34B - Sale.cpp
@@ -1,25 +1,26 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-#define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+#define IOS ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 #define TEST int t = 0; cin >> t; while(t--)
+#define ll long long
 
 
 int main()
 {
     IOS
-    int n = 0, m = 0, s = 0;
+    int n  = 0, m = 0, ans = 0;
     cin >> n >> m;
     int a[n] = {0};
     for(int i = 0; i < n; i++)
         cin >> a[i];
     sort(a, a + n);
-    int j = 0;
-    while(j < m && a[j] <= 0){
-        s += a[j];
-        j++;
+    for(int i = 0; i < m; i++){
+        if(a[i] >= 0)
+            break;
+        ans -= a[i];
     }
-    s *= -1;
-    cout << s;
+    cout << ans;
     return 0;
 }
