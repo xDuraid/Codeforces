fa3dd3cddadd4ebac77fb1dd6b9dedbe0861b554
Duraid Maihoub <73091719+xDuraid@users.noreply.github.com>
2021-09-16 09:56:07 -0600

    Create 1283B - Candies Division.cpp

---
+++ 1283B - Candies Division.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+#define IOS ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define TEST int t = 0; cin >> t; while(t--)
+#define ll long long
+#define sz size()
+#define fi first
+#define se second
+#define IGNORE cin.ignore(numeric_limits<stream_size>::max(), '\n');
+
+int main()
+{
+    IOS
+
+    TEST
+    {
+        int n = 0, k = 0, ans = 0;
+        cin >> n >> k;
+        ans = n - n % k + min(n % k, k / 2);
+        cout << ans << endl;
+    }
+    return 0;
+}
