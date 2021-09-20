2882da01fddbee79b55bb2f99a10db364c188870
Duraid Maihoub <73091719+xDuraid@users.noreply.github.com>
2021-09-20 10:01:01 -0600

    Create 913A - Modular Exponentiation.cpp

---
+++ 913A - Modular Exponentiation.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+#define IOS ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define TEST int tst = 0; cin >> tst; while(tst--)
+#define ll long long
+#define ln length()
+#define fi first
+#define se second
+#define IGNORE cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+int main()
+{
+    IOS
+    int n = 0, m = 0;
+    cin >> n >> m;
+    if(n > 26)
+        cout << m;
+    else{
+        n = (1 << n);
+        cout << m % n;
+    }
+    return 0;
+}
