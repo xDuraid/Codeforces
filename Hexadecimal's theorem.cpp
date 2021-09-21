a7a8077df0a8ba42923b8b74bcc4601becc3ad4d
Duraid Maihoub <73091719+xDuraid@users.noreply.github.com>
2021-09-20 18:51:45 -0600

    Create Hexadecimal's theorem.cpp

---
+++ Hexadecimal's theorem.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+#define IOS ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define TEST int tst = 0; cin >> tst; while(tst--)
+#define ll long long
+#define ln length()
+#define fi first
+#define se second
+#define IGNORE cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+
+int main()
+{
+    IOS
+    int n = 0;
+    cin >> n;
+    cout << "0 0 " << n;
+    return 0;
+}
